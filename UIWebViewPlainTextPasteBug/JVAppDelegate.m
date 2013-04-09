edc16daa68f3759eedf1cc7d4ee7ec709e85ed86
Heath Borders <heath.borders@jivesoftware.com>
2013-04-08 23:40:06 -0500

    Demonstrates UIWebView paste bug.

--- UIWebViewPlainTextPasteBug/JVAppDelegate.m
+++ UIWebViewPlainTextPasteBug/JVAppDelegate.m
@@ -15,35 +15,23 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    UIWebView *webView = [UIWebView new];
+    [webView loadHTMLString:@"<html><body><div><button onclick=\"alert(document.getElementById('red').innerHTML)\">show red</button><button onclick=\"alert(document.getElementById('blue').innerHTML)\">show blue</button></div><div id=\"red\" contenteditable=\"true\" style=\"background-color: red; font-size: 32px;\"></div><div id=\"blue\" contenteditable=\"true\" style=\"background-color: blue;\"></div></body></html>" baseURL:nil];
+    
+    UIViewController *viewController = [UIViewController new];
+    viewController.view = webView;
+    
+    self.window.rootViewController = viewController;
     [self.window makeKeyAndVisible];
+    
+    [[[UIAlertView alloc] initWithTitle:@"Directions"
+                                message:@"1. Paste text in the red rectangle, click the 'show red' button\n2. Paste text in the blue rectangle, click the 'show blue' button.\nNotice that red is wrapped in <font size='3'>, but blue is not.\n\nThis happens in iOS6+, but not in iOS5.1-"
+                               delegate:nil
+                      cancelButtonTitle:@"OK"
+                      otherButtonTitles:nil] show];
+    
     return YES;
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
