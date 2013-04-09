fb27dd56ac0001afd0362291faaea239850bc92a
Heath Borders <heath.borders@jivesoftware.com>
2013-04-08 22:45:21 -0500

    Initial Commit

---
+++ UIWebViewPlainTextPasteBug/JVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JVAppDelegate.h
+//  UIWebViewPlainTextPasteBug
+//
+//  Created by Heath Borders on 4/8/13.
+//  Copyright (c) 2013 Jive Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIWebViewPlainTextPasteBug/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UIWebViewPlainTextPasteBug
+//
+//  Created by Heath Borders on 4/8/13.
+//  Copyright (c) 2013 Jive Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JVAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JVAppDelegate class]));
+    }
+}
