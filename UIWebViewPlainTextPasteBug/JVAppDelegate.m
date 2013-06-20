09d386b3b513e33e1eecae921a83e80531f207b2
Heath Borders <heath.borders@jivesoftware.com>
2013-06-20 10:33:09 -0500

    Improved directions for reproducibility.

--- UIWebViewPlainTextPasteBug/JVAppDelegate.m
+++ UIWebViewPlainTextPasteBug/JVAppDelegate.m
@@ -17,7 +17,7 @@
     self.window.backgroundColor = [UIColor whiteColor];
     
     UIWebView *webView = [UIWebView new];
-    [webView loadHTMLString:@"<html><body><div><button onclick=\"alert(document.getElementById('red').innerHTML)\">show red</button><button onclick=\"alert(document.getElementById('blue').innerHTML)\">show blue</button></div><div id=\"red\" contenteditable=\"true\" style=\"background-color: red; font-size: 32px;\"></div><div id=\"blue\" contenteditable=\"true\" style=\"background-color: blue;\"></div></body></html>" baseURL:nil];
+    [webView loadHTMLString:@"<html><body><div><button onclick=\"alert(document.getElementById('red').innerHTML)\">show red</button><button onclick=\"alert(document.getElementById('blue').innerHTML)\">show blue</button></div><div id=\"red\" contenteditable=\"true\" style=\"background-color: red; font-size: 32px;\"></div><div id=\"blue\" contenteditable=\"true\" style=\"background-color: blue;\"></div><div id=\"copyme\" style=\"margin-top:100\">Copy Me</div></body></html>" baseURL:nil];
     
     UIViewController *viewController = [UIViewController new];
     viewController.view = webView;
@@ -26,7 +26,7 @@
     [self.window makeKeyAndVisible];
     
     [[[UIAlertView alloc] initWithTitle:@"Directions"
-                                message:@"1. Paste text in the red rectangle, click the 'show red' button\n2. Paste text in the blue rectangle, click the 'show blue' button.\nNotice that red is wrapped in <font size='3'>, but blue is not.\n\nThis happens in iOS6+, but not in iOS5.1-"
+                                message:@"1. Copy the \"Copy Me\" Text.\n2. Paste text in the red rectangle, click the 'show red' button\n3. Paste text in the blue rectangle, click the 'show blue' button.\nNotice that red is wrapped in <font size='3'>, but blue is not.\n\nThis happens in iOS6+, but not in iOS5.1-"
                                delegate:nil
                       cancelButtonTitle:@"OK"
                       otherButtonTitles:nil] show];
